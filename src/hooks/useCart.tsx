104270553467306b675723eba0976ad048380a55
pedro leinar <pedroleinar@hotmail.com>
2021-04-19 05:50:52 -0300

    feat: add product

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,20 +23,43 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const productExist = cart.find((p) => p.id === productId);
+      const stock = (await api.get<{ amount: number }>(`/stock/${productId}`)).data;
+
+      const updatedCart = [...cart];
+
+      const product_amount = (productExist?.amount ?? 0) + 1;
+
+      if (stock.amount < product_amount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      if (productExist) {
+        productExist.amount = product_amount;
+      } else {
+        const product = (await api.get<Product>(`/products/${productId}`)).data;
+
+        updatedCart.push({ ...product, amount: 1 });
+      }
+
+      setCart(updatedCart);
+
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
     } catch {
-      // TODO
+      toast.error('Erro na adição de produto');
+      return;
     }
   };
 
@@ -48,10 +71,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     }
   };
 
-  const updateProductAmount = async ({
-    productId,
-    amount,
-  }: UpdateProductAmount) => {
+  const updateProductAmount = async ({ productId, amount }: UpdateProductAmount) => {
     try {
       // TODO
     } catch {
@@ -60,9 +80,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   };
 
   return (
-    <CartContext.Provider
-      value={{ cart, addProduct, removeProduct, updateProductAmount }}
-    >
+    <CartContext.Provider value={{ cart, addProduct, removeProduct, updateProductAmount }}>
       {children}
     </CartContext.Provider>
   );
